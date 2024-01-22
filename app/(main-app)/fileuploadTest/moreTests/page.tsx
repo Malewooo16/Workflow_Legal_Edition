dc38d289678d039b50435ee8aa2ab86c39c2f3fa
malewoodev <elijahmalewo16@gmail.com>
2024-01-22 19:02:11 +0300

    merged

--- app/(main-app)/fileuploadTest/moreTests/page.tsx
+++ app/(main-app)/fileuploadTest/moreTests/page.tsx
@@ -1,59 +1,87 @@
 "use client"
 
-import { useForm } from "react-hook-form";
-import {yupResolver} from '@hookform/resolvers/yup'
-import * as Yup from 'yup';
+
+import { useState } from "react";
+import { addNewUser } from "@/app/actions/testActions/addUser";
 
 export default function page() {
-    const validationSchema = Yup.object().shape({
-        firstName: Yup.string().required('Title is required'),
-        lastName: Yup.string().required('Description is required'),
-        townAddress: Yup.string().required('Deadline is required'),
-        emailAddress: Yup.string().required('Collaborators are required'),
-        dob:Yup.string().required("Add Date of Birth")
-      
-      });
+   
+   const [base64String, setBase64String] = useState <string | null>('');
+   const [validationErrors , setValidationErrors] = useState <string>('')
+    const handleFileChange = (event:any) => {
+      const file = event.target.files[0];
+  
+      if (file) {
+        // Read the file as a data URL
+        const reader = new FileReader();
+  
+        reader.onloadend = () => {
+          // Set the Base64 string in the state
+          const base64 = reader.result as string
+          setBase64String(base64);
+        };
+  
+        reader.readAsDataURL(file);
+      }
+    };
+    
+  const uploadPerson = async (formData:FormData) =>{
 
-      const { register, handleSubmit, reset, formState: { errors } } = useForm({
-        resolver: yupResolver(validationSchema),
-      });
+    const userUploadResponse = await addNewUser(formData);
+
+    if(userUploadResponse.success==false && userUploadResponse.type == 'validation'){
+      setValidationErrors(userUploadResponse.error)
+    }
+
+  }
+      
+
+      
   return (
     <div>
-    <form>
+    <form action={uploadPerson} className="mx-6">
+      <h2 className="text-2xl">User Upload</h2>
     <div className="join join-vertical flex">
     <label className='form-control max-w-xl my-4 join-item'>
-           <p className="my-2"> FirstName</p>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full "  {...register("lastName")}/>
-       <p className="text-error text-sm"> {errors.lastName?.message} </p>
+           <p className="my-2"> Last Name</p>
+        <input type="text" placeholder="Type here" className="input input-bordered w-full "  name="lastName"/>
+       <p className="text-error text-sm">  </p>
         </label>
         <label className='form-control max-w-xl my-4 join-item'>
-           <p className="my-2"> FirstName</p>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full "  {...register("firstName")}/>
-       <p className="text-error text-sm"> {errors.firstName?.message} </p>
+           <p className="my-2"> First Name</p>
+        <input type="text" placeholder="Type here" className="input input-bordered w-full "  name="firstName"/>
+       <p className="text-error text-sm"> </p>
         </label>
         <label className='form-control max-w-xl my-4 join-item'>
            <p className="my-2"> Email Address</p>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full "  {...register("lastName")}/>
-       <p className="text-error text-sm"> {errors.lastName?.message} </p>
+        <input type="text" placeholder="Type here" className="input input-bordered w-full "  name="emailAddress"/>
+       <p className="text-error text-sm">  </p>
         </label>
         <label className='form-control max-w-xl my-4 join-item'>
            <p className="my-2"> Town Address</p>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full "  {...register("townAddress")}/>
-       <p className="text-error text-sm"> {errors.townAddress?.message} </p>
+        <input type="text" placeholder="Type here" className="input input-bordered w-full "  name="townAddress"/>
+       <p className="text-error text-sm">  </p>
         </label>
 
         <label className='form-control max-w-xl my-4 join-item'>
         <p className="my-2"> Date of Birth</p>
-        <input type="datetime-local"  max={new Date().toISOString().slice(0,16)} placeholder="Type here" className="input input-bordered w-full "  {...register("dob")}/>
-        <p className="text-error text-sm"> {errors.dob?.message} </p>
+        <input type="date"  max={new Date().toISOString().slice(0,10)} placeholder="Type here" className="input input-bordered w-full "  name="dob"/>
+        <p className="text-error text-sm">  </p>
         </label>
 
         <label className='form-control max-w-xl my-4 join-item '>
-           <p className="my-2"> Town Address</p>
-           <input type="file" className="file-input file-input-bordered w-full " />
-       
+           <p className="my-2"> Picture</p>
+           <input type="file" className="file-input file-input-bordered w-full " onChange={handleFileChange} name="userPic" />
+           {base64String && (
+        <div>
+          
+          <p>Image Preview:</p>
+          <img src={base64String} alt="Preview" />
+        </div>
+      )}
         </label>
-        
+        {validationErrors && <p className= 'text-error'> {validationErrors} </p>}
+        <button type="submit" className="btn btn-success">Add User</button>
     </div>
     </form>
     
---
+++ app/actions/testActions/addUser.ts
@@ -0,0 +1,97 @@
+
+"use server"
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import prisma from "@/app/db/prismadb";
+import toB2Test from "./toB2Test";
+import imgUpload from "./testPictureUpload";
+import * as Yup from 'yup';
+
+interface SuccessResponse {
+  success: true;
+  message?: string;
+}
+
+interface ErrorResponse {
+  type:string;
+  success: false;
+  error: string;
+}
+
+
+interface UserData {
+  firstName: string;
+  lastName: string;
+  dob: string;
+  townAddress: string;
+  emailAddress: string;
+  userPic: File;
+}
+
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string().required('First Name is required'),
+  lastName: Yup.string().required('Last Name is required'),
+  dob: Yup.date().required('Date of Birth is required').typeError('Invalid Date'),
+  townAddress: Yup.string().required('Town Address is required'),
+  emailAddress: Yup.string().email('Invalid email address').required('Email Address is required'),
+});
+
+export async function addNewUser(formData: FormData): Promise<SuccessResponse | ErrorResponse > {
+  try {
+    await validationSchema.validate({
+      firstName: formData.get("firstName") as string,
+      lastName: formData.get("lastName") as string,
+      dob: formData.get("dob") as string,
+      townAddress: formData.get("townAddress") as string,
+      emailAddress: formData.get("emailAddress") as string,
+    }, { abortEarly: false });
+
+    const rawFormData: UserData = {
+      firstName: formData.get("firstName") as string,
+      lastName: formData.get("lastName") as string,
+      dob: formData.get("dob") as string,
+      townAddress: formData.get("townAddress") as string,
+      emailAddress: formData.get("emailAddress") as string,
+      userPic: formData.get("userPic") as File,
+    };
+
+    // Create a FormData instance
+
+    // Parse the deadline string into a JavaScript Date object
+    const updatedDob = new Date(rawFormData.dob);
+    console.log(rawFormData.userPic.name);
+    const b2Response = await imgUpload(rawFormData.userPic);
+    const imgUrl = b2Response?.location;
+    const updatedData = {
+      firstName: rawFormData.firstName,
+      lastName: rawFormData.lastName,
+      emailAddress: rawFormData.emailAddress,
+      townAddress: rawFormData.townAddress,
+      dob: updatedDob,
+      pictureUrl: imgUrl,
+      
+    };
+
+    const prismaResponse = await prisma.testUser.create({
+      data: updatedData,
+    });
+
+    console.log(prismaResponse);
+
+    return { success: true, message: "User created successfully." };
+  } catch (error) {
+    console.error('Error creating user:', error);
+
+    if (error instanceof Yup.ValidationError) {
+      // Yup validation error
+      const validationErrors = error.inner.map(err => ({
+        field: err.path,
+        message: err.message,
+      }));
+      console.log(validationErrors);
+      return { type:"validation", success: false, error: JSON.stringify(validationErrors) };
+    }
+    
+    return { type: "generic", success: false, error: "Failed to create user." };
+  }
+}
